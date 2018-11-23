01a3e2cf13996be2a6b7c2364fb45879bd98dcc3
aimeshuu <guillaume.cavallero@yahoo.fr>
2018-11-23 10:54:39 +0100

    initialized basic django 2.1 project with models

---
+++ basicform/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class basicform(models.Model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    phone = models.CharField(max_length=50)
+    subject = models.CharField(max_length = 255)
+    message = models.TextField
+    checkbox =models.BooleanField
+    
+    
+    
+    
